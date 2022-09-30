30711b8e750e5ba5efcf779eee531f9f6cdba423
Ahmed Nser El-Din <72213733+Ahmed-Naserelden@users.noreply.github.com>
2022-09-30 23:44:42 +0200

    b 4.2

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,6 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
 
-
-
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
--- lib/modules/products/product_screen.dart
+++ lib/modules/products/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopping/layout/home_layout/cubit/shop_cubit.dart';
@@ -10,14 +11,25 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<ShopCubit, ShopStatus>(
         builder: (context, state){
-          return Center(
-            child: Text('Product Screen',
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
+          return ConditionalBuilder(
+            condition: ShopCubit.get(context).homeDataModel != null,
+            builder: (context) => productBuilder(),
+            fallback: (context) => const Center(child: CircularProgressIndicator()),
           );
         },
-        listener: (context, state){}
+        listener: (context, state){
+
+        }
     );
 
   }
+
+  Widget productBuilder(){
+    return Column(
+      children: const [
+
+      ],
+    );
+  }
+
 }
